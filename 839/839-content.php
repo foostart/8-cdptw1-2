9b32cc7232026fd7fe2807dad214b55d911f337c
foostart.com <foostart.com@gmail.com>
2019-09-12 08:23:25 +0700

    Cm

---
+++ 839/839-content.php
@@ -0,0 +1,67 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-839">
+    <div class="container">
+        <div class="Boderfull row">
+            <p><strong>BLOG</strong></p>
+            <div class="col-md-4 col-xs-12 col-sm-6 wrap_border">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="http://<?php echo $url_path ?>/images/blog_1_1024x1024.jpg" alt="blog1-responsive">
+                </div>
+                <div class="font-name">
+                    <h1 class="hidden-sm-xs"><a href="#">Blog</a></h1>
+                    <h2><a href="#">We are providing a great choice of different commodities</a></h2>
+                    <br>
+                    <h3 class="hidden-sm-xs">We can help you solve any problem because we provide only rational solutions....</h3>
+                    <br>
+                    <h4><i class="fa fa-clock-o"></i> July 11, 2016</h4>     
+                </div>
+                <h5><a class="btn btnshopnow"> <strong>Read More</strong></a></h5>
+            </div>
+            <div class="col-md-4 col-xs-12 col-sm-6 wrap_border">
+                <div class="thumbnail">
+                    <img src="http://<?php echo $url_path ?>/images/blog_2_1024x1024.jpg" alt="blog2-responsive">
+                </div>
+                <div class="font-name">
+                    <h1 class="hidden-sm-xs"><a href="#"> Blog</a></h1>
+                    <h2><a href="#">We can satisfy customers with different claims</a></h2>
+                    <br>
+                    <h3 class="hidden-sm-xs">So, it is natural that this sphere is one of the most popular and it is really hard to offer...</h3>
+                    <br>
+                    <h4><i class="fa fa-clock-o" ></i> July 11, 2016</h4>
+
+                </div>
+                <h5><a class="btn btnshopnow"> <strong>Read More</strong></a></h5>
+            </div>
+            <div class="col-md-4 col-xs-12 col-sm-6 wrap_border">
+                <div class="thumbnail">
+                    <img src="http://<?php echo $url_path ?>/images/blog_3_1024x1024.jpg" alt="blog3-responsive">
+                </div>
+                <div class="col-sm-12 font-name">
+                    <h1 class="hidden-sm-xs">Blog</h1>
+                    <h2><a href="#">We can help you solve any problem because we provide only...</a></h2>
+                    <br>
+                    <h3 class="hidden-sm-xs">We are living in the epoch of great technical progress....</h3>
+                    <br>
+                    <h4><i class="fa fa-clock-o" ></i> July 11, 2016</h4>
+
+                </div>
+                <h5><a class="btn btnshopnow"> <strong>Read More</strong></a></h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+   
---
+++ 839/839.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+if (!class_exists('lessc')) {
+    $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);
+    require_once($dir_block . '/libs/lessc.inc.php');
+}
+$less = new lessc;
+$less->compileFile('less/839.less', 'css/839.css');
+?>
+<html lang="en">
+    <head>
+        <title>839</title>
+        <meta charset="utf-8">
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Basic usage demo">
+        <meta name="author" content="David Deutsch">
+        <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/839.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <?php include '../839/839-content.php'; ?>
+    </body>
+</html>
