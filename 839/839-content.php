8a5ed2bad896d659b12ed55b842981eff5ff56a3
Joseph Hieu <hieunguyenductt6@gmail.com>
2019-09-28 10:35:38 +0700

    commit

--- 839/839-content.php
+++ 839/839-content.php
@@ -14,39 +14,38 @@ $url_path = str_replace('\\', '/', $url_path);
 <div class="type-839">
     <div class="container boderfull">
         <p><strong>BLOG</strong></p>
-        <div class="card-group">
+        <div class="card-columns">
             <div class="card">
-                <div class="thumbnail">
-                  <img class="card-img-top" src="http://<?php echo $url_path ?>/images/blog_1_1024x1024.jpg" alt="blog1-responsive">  
-                </div>  
-                
+                <img class="card-img-top" src="http://<?php echo $url_path ?>/images/blog_1_1024x1024.jpg" alt="blog1-responsive">
                 <div class="card-body">
-                    <h3 class="card-title"><a href="#">Blog</a></h3>
-                    <h4><a href="#">We are providing a great choice of different commodities</a></h4>
+                    <div class="header">
+                        <a href="#">Blog</a>
+                    </div>
+                    <h4 class="card-title">TWe are providing a great choice of different commodities</h4>
                     <p class="card-text">We can help you solve any problem because we provide only rational solutions....</p>
                     <h5><i class="fa fa-clock-o"></i> July 11, 2016</h5>   
                     <a href="#" class="btn btn-primary">Read More</a>
                 </div>
             </div>
             <div class="card">
-                <div class="thumbnail">
-                  <img class="card-img-top" src="http://<?php echo $url_path ?>/images/blog_1_1024x1024.jpg" alt="blog1-responsive">  
-                </div>
+                <img class="card-img-top" src="http://<?php echo $url_path ?>/images/blog_1_1024x1024.jpg" alt="blog1-responsive">
                 <div class="card-body">
-                    <h3 class="card-title"><a href="#">Blog</a></h3>
-                    <h4><a href="#">We are providing a great choice of different commodities</a></h4>
+                    <div class="header">
+                        <a href="#">Blog</a>
+                    </div>
+                    <h4 class="card-title">TWe are providing a great choice of different commodities</h4>
                     <p class="card-text">We can help you solve any problem because we provide only rational solutions....</p>
                     <h5><i class="fa fa-clock-o"></i> July 11, 2016</h5>   
                     <a href="#" class="btn btn-primary">Read More</a>
                 </div>
             </div>
             <div class="card">
-                <div class="thumbnail">
-                  <img class="card-img-top" src="http://<?php echo $url_path ?>/images/blog_1_1024x1024.jpg" alt="blog1-responsive">  
-                </div>
+                <img class="card-img-top" src="http://<?php echo $url_path ?>/images/blog_1_1024x1024.jpg" alt="blog1-responsive">
                 <div class="card-body">
-                    <h3 class="card-title"><a href="#">Blog</a></h3>
-                    <h4><a href="#">We are providing a great choice of different commodities</a></h4>
+                    <div class="header">
+                        <a href="#">Blog</a>
+                    </div>
+                    <h4 class="card-title">TWe are providing a great choice of different commodities</h4>
                     <p class="card-text">We can help you solve any problem because we provide only rational solutions....</p>
                     <h5><i class="fa fa-clock-o"></i> July 11, 2016</h5>   
                     <a href="#" class="btn btn-primary">Read More</a>
--- 839/839.php
+++ 839/839.php
@@ -23,6 +23,10 @@ $less->compileFile('less/839.less', 'css/839.css');
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Basic usage demo">
         <meta name="author" content="David Deutsch">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+        crossorigin="anonymous">
         <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/839.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
